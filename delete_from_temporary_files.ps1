72707da4c95d35dddf59a02321d415742a05acbd
Kresimir Ivkovic <kresimir.ivkovic@gmail.com>
2025-02-06 17:35:32 +0100

    today

--- delete_from_temporary_files.ps1
+++ delete_from_temporary_files.ps1
@@ -1,11 +1,13 @@
 # Delete not in-use *.tmp files
 
 echo " Delete not in-use *.tmp files "
-$FilesToRemove = Get-ChildItem -Path C:\Windows -Include *.tmp, *.etl -Recurse -ErrorAction SilentlyContinue
+# $FilesToRemove = Get-ChildItem -Path C:\Windows -Include *.tmp, *.etl -Recurse -ErrorAction SilentlyContinue
+$FilesToRemove = Get-ChildItem -Path D:\Test -Include *.tmp, *.etl -Recurse -ErrorAction SilentlyContinue
 echo " List created successfully"
-break;
+echo $FilesToRemove
 $FilesToRemove | Remove-Item -ErrorAction SilentlyContinue
 echo "+ State 1"
+break;
 
 # Delete not in-use anything in your %temp% folder
 echo " Delete not in-use anything in your %temp% folder "
